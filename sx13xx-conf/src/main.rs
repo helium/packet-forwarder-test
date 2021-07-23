dfd990e1ea64b177fe75e65bd7b6eb0be1703cd9
lthiery <thiery.louis@gmail.com>
2021-07-23 11:40:16 -0700

    better help menu

--- sx13xx-conf/src/main.rs
+++ sx13xx-conf/src/main.rs
@@ -8,11 +8,20 @@ mod global_conf;
 use global_conf::*;
 
 #[derive(Debug, StructOpt)]
-/// Provide file input
+/// Tests the frequency configuration of a SX1301
+/// or SX1302 configuration file (global_conf.json)
 pub struct Opt {
-    #[structopt(name = "CONF_PATH", required = true)]
+    /// Path to global_conf.json under test. SX1301
+    /// and SX1302 configuration files are acceptable
+    /// Comments (eg: "//" or "/* */ and variables
+    /// (eg: ${VAR}) are stripped out before parsing
+    #[structopt(name = "path_to_conf", required = true)]
     path: String,
-    #[structopt(long, short)]
+    /// Selection region to test against. Options are:
+    /// US915, EU868, EU433, CN470, CN779, AU915,
+    /// AS923_1, AS923_2, AS923_3, AS923_4, KR920,
+    /// IN865, RU864
+    #[structopt(required = true)]
     region: Region,
 }
 
